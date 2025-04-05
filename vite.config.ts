dd0faada287b3f5f8ad09e2957ca5a075b5760ee
Fanuel Israel TOVEY <fanueldev2.0@gmail.com>
2025-04-05 16:40:06 +0000

    cc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.htaccess',      // fichier source à la racine du projet
+          dest: ''               // destination dans le dossier dist
+        }
+      ]
+    }),],
 })
