0298086470a40be84939bb0278b7b8ea3a998361
Fanuel Israel TOVEY <fanueldev2.0@gmail.com>
2025-07-04 16:27:31 +0000

    cc

--- src/pages/admin/reservation.vue
+++ src/pages/admin/reservation.vue
@@ -3,13 +3,12 @@
     <h5 class="fw-bold mb-4 pb-2">Liste des réservations</h5>
     <a-table :columns="reservationColumns" :data-source="reservations" row-key="id" size="middle">
       <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'piece_justificative'">
+          <a-button type="link" @click="viewFile(record.piece_justificative)">Voir</a-button>
+        </template>
         <template v-if="column.key === 'actions'">
-          <a-popconfirm
-            title="Valider cette réservation ?"
-            ok-text="Oui"
-            cancel-text="Non"
-            @confirm="validateReservation(record.id)"
-          >
+          <a-popconfirm title="Valider cette réservation ?" ok-text="Oui" cancel-text="Non"
+            @confirm="validateReservation(record.id)">
             <a-button type="primary" size="small">Valider</a-button>
           </a-popconfirm>
         </template>
@@ -31,11 +30,14 @@ interface Reservation {
   statut: string
 }
 
+const baseUrlFront = import.meta.env.VITE_FRONT_URL
+
 const reservations = ref<Reservation[]>([])
 
 const reservationColumns = [
   { title: 'Date de réservation', dataIndex: 'dateReservation', key: 'dateReservation' },
   { title: 'Client', dataIndex: 'client', key: 'client' },
+  { title: 'Pièce d’identité', dataIndex: 'piece_justificative', key: 'piece_justificative' },
   { title: 'Voiture', dataIndex: 'voiture', key: 'voiture' },
   { title: 'Date de debut', dataIndex: 'dateDebut', key: 'dateDebut' },
   { title: 'Date de fin', dataIndex: 'dateFin', key: 'dateFin' },
@@ -43,6 +45,12 @@ const reservationColumns = [
   { title: 'Statut', dataIndex: 'statut', key: 'statut' },
   { title: 'Actions', key: 'actions' },
 ]
+
+
+function viewFile(url: string) {
+  console.log(url)
+  window.open(`${baseUrlFront}/${url}`, '_blank')
+}
 
 const validateReservation = async (id: number) => {
   apiService.adminValidReservation(id).then(res => {
@@ -71,6 +79,7 @@ onMounted(() => {
 .shadow {
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.05);
 }
+
 .rounded {
   border-radius: 0.75rem;
 }
